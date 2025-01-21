c83009c9636a541f4a0b50d57f4aff2334e7ec6b
MR-S4NM1 <miguelitogarcia30@outlook.com>
2025-01-20 19:41:06 -0600

    Input Update_20-01-2025

---
+++ Assets/Bubble_Journey/Code/UI/UIManager.cs
@@ -0,0 +1,74 @@
+using MrSanmiAndNAwakening.BubbleJourney;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MrSanmiAndNAwakening.BubbleJourney
+{
+    public class UIManager : MonoBehaviour
+    {
+        #region References
+
+        public static UIManager instance;
+
+        [SerializeField] protected GameObject _gamePanel;
+        [SerializeField] protected GameObject _defeatPanel;
+        [SerializeField] protected GameObject _pausePanel;
+        [SerializeField] protected TextMeshProUGUI _hpNarrator;
+        [SerializeField] protected RS_MobileInputHandler _mobileInputHandler;
+
+        #endregion
+
+        #region RuntimeVariables
+
+        #endregion
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else
+            {
+                Destroy(this);
+            }
+        }
+
+        private void Start()
+        {
+            _gamePanel.SetActive(true);
+            _defeatPanel.gameObject.SetActive(false);
+            //_hpNarrator.text = _gameReferee.GetHealthPoints.ToString();
+        }
+
+        public void ActivateDefeatPanel()
+        {
+            _gamePanel.gameObject.SetActive(false);
+            _defeatPanel.gameObject.SetActive(true);
+        }
+
+        public void ActivateAndDeactivatePausePanel(bool pausePanelBool)
+        {
+            switch (pausePanelBool)
+            {
+                case true:
+                    _gamePanel.gameObject.SetActive(false);
+                    _pausePanel.gameObject.SetActive(true);
+                    break;
+                case false:
+                    _gamePanel.gameObject.SetActive(true);
+                    _pausePanel.gameObject.SetActive(false);
+                    break;
+            }
+        }
+
+        public void UpdatePlayerLife (int playerHP)
+        {
+            _hpNarrator.text = playerHP.ToString();
+        }
+    }
+}
+
